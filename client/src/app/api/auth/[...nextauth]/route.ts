195556dd4778b00b9ae0f31b5737a73f347aabd5
Alejandro Urroz <alejandrourroz86@gmail.com>
2023-12-12 02:24:42 +0100

    Settet up oauth with next-auth google provider

---
+++ client/src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,52 @@
+import NextAuth from 'next-auth/next';
+import GoogleProvider from 'next-auth/providers/google';
+import { NextAuthOptions } from 'next-auth';
+import { db } from '../../../../../firebase'; // Adjust the path as needed
+import { doc, setDoc, getDoc } from 'firebase/firestore';
+
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID!;
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET!;
+
+const authOptions: NextAuthOptions = {
+  session: {
+    strategy: 'jwt',
+  },
+  providers: [
+    GoogleProvider({
+      clientId: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      if (!profile?.email) {
+        throw new Error('No profile');
+      }
+      const userData = {
+        name: profile.name || user.name, // Fallback to `user.name` if `profile.name` isn't available
+        email: profile.email,
+      };
+
+      const userRef = doc(db, 'users', profile.email);
+
+      try {
+        const userSnap = await getDoc(userRef);
+
+        if (!userSnap.exists()) {
+          await setDoc(userRef, userData);
+        } else {
+          await setDoc(userRef, userData, { merge: true });
+        }
+      } catch (error) {
+        console.error('Error updating user in Firestore', error);
+        throw new Error('Error updating user data');
+      }
+
+      return true;
+    },
+  },
+};
+
+export const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
